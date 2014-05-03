aa5ac9922e15e38b177c347ed4791310d406fedb
paul <paulthomas1987@gmail.com>
2014-05-03 23:26:25 +0100

    Fixing global reference2

--- setup.py
+++ setup.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 import os, subprocess, sys
-subprocess.call(['python', 'virtualenv.py', 'flask'])
+# subprocess.call(['python', 'virtualenv.py', 'flask'])
 if sys.platform == 'win32':
     bin = 'Scripts'
 else:
     bin = 'bin'
-subprocess.call([os.path.join('flask', bin, 'pip'), 'install', 'flask<0.11'])
